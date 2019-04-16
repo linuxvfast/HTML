63d8deadc3dbe94e7d6dade5b0838b0cbaad9ff7
soft <2559811312@qq.com>
2019-04-16 15:14:55 +0800

    Add files via upload

---
+++ django416/django416/urls.py
@@ -0,0 +1,27 @@
+"""django416 URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from cmsdb import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    # path('index.html/',views.index),
+    path('login',views.login), #Current login path with the login. The HTML file is consistent with the action\
+    # （当前的login要与login.html文件中的action保持一致，login后要不加/都不加，要加需要都加）
+    path('home',views.home),
+]
+
